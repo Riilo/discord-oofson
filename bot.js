559319cf61d9857377ac91b5bb1e535a847de6f6
Riilo <rilomilo9@hotmail.com>
2018-04-24 18:11:11 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ client.on("guildCreate", guild => {
 });
 
 client.on("messageDelete", (messageDelete) => {
- messageDelete.channel.send(`The message: "${messageDelete.content}" by ${messageDelete.author.tag} was deleted. Let's hope you're better at hiding your porn.`)
- message.author.sendMessage(`The message you sent: "${messageDelete.content}" by ${messageDelete.author.tag} was deleted. Let's hope you're better at hiding your porn.`)
+ messageDelete.channel.send(`The message: "${messageDelete.content}" by ${messageDelete.author.tag} was deleted. Let's hope you're better at hiding your porn.`);
+ message.author.sendMessage(`The message you sent: "${messageDelete.content}" by ${messageDelete.author.tag} was deleted. Let's hope you're better at hiding your porn.`);
 });
  
 
