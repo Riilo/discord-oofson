0561fedde3836ec7820557e9d77a73f6f43f966f
Riilo <rilomilo9@hotmail.com>
2018-05-05 08:20:36 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,14 @@ client.on("guildDelete", guild => {
   client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
+if(command === "stats") {
+    member.guild.channels.get('439792255365021696').setName(`Total Users: ${member.guild.memberCount}`)
+    let humans = member.guild.members.filter(m => !m.user.bot).size;
+    member.guild.channels.get('439793088001736725').setName(`Member Count: ${humans}`)
+    let bots = member.guild.members.filter(m => m.user.bot).size;
+    member.guild.channels.get('439793716052623361').setName(`Bot Count: ${bots}`)
+});
+
 client.on('message', (message) => {
       if(message.content.startsWith('!help')) {
           message.channel.send({embed: {
