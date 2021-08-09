cf32d1c2a756acafeb8e63d667da6516f6fa74a8
Riilo <rilomilo9@hotmail.com>
2021-08-09 12:19:42 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,16 +33,12 @@ client.on('message', (message) => {
               color: Math.floor(Math.random() * 16777214) + 1,  
               description: ("Welcome to the Bettsey Bot **HELP PAGE!**\n"),
               fields: [{
-                name: "Removing users: ↓",
-                value: "`!ban <user (ping/mention)> <reason>` - Bans the user specified from the server.\n———\n`!kick <user (ping/mention)> <reason>` - Kicks the user specified from the server.\n"
-              },
-              {
                 name: "Personal Commands: ↓",
-                value: "`!ping` - Gives you your ping.\n———\n`!poll` - Creates a poll with answers 'Yes' and 'No'\n———\n`!help` - Makes this help page you are currently looking at.\n"
+                value: "`!help` - Makes this help page you are currently looking at.\n"
               },
               {
                 name: "Others: ↓",
-                value: "`!purge <2-100>` - Clears chat history depending on how many messages specified.\n———\n`!say <message>` - Controls what the bot says.\n"
+                value: "`!say <message>` - Controls what the bot says.\n"
               }
             ],
             timestamp: new Date(),
@@ -66,30 +62,6 @@ client.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-  if(command === "ping") {
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  } else
-
-  if(command === "game") {
-    const game = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    client.user.setGame(game);
-
-  } else
-
-  if(command === "bot") {
-    member.guild.channels.get('439792255365021696').setName(`Total Users: ${member.guild.memberCount}`)
-    let humans = member.guild.members.filter(m => !m.user.bot).size;
-    member.guild.channels.get('439793088001736725').setName(`Member Count: ${humans}`)
-    let bots = member.guild.members.filter(m => m.user.bot).size;
-    member.guild.channels.get('439793716052623361').setName(`Bot Count: ${bots}`)
-  }
-
-  if(command === "avatar") {
-  client.users.get(event.user.uid).avatarURL
-  
-  }
 
   if(command === "say") {
     const sayMessage = args.join(" ");
@@ -97,75 +69,6 @@ client.on("message", async message => {
     message.channel.send(sayMessage);
   }
 
-  if(command === "info") {
-    message.channel.send({embed: {
-    color: 3447003,
-    icon_url: client.user.avatarURL,
-    description: "G'day, I'm Oofson, the biggest meme of 2018. I was created by the legendary @Rilo#6659. I like rushing to class and being a teacher pet. Which is why I have come to help you all and provide you with some amazing commands. Do '!help' to see all of the commands."
-}})};
-  
-
-  if(command === "poll") {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    const pollTopic = await message.channel.send("**Incoming Poll: **"+"`"+sayMessage+"`");
-    pollTopic.react(`✅`);
-    pollTopic.react(`⛔`);
-};
-// test
-  
-  if(command === "kick") {
-    if(!message.member.roles.some(r=>["Owner","Colonel General"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    let reason = args.slice(1).join(' ');
-    if(!reason)
-      return message.reply("Please indicate a reason for the kick!");
-    
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-  }
-
-  
-  if(command === "ban") {
-    if(!message.member.roles.some(r=>["Owner"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason)
-      return message.reply("Please indicate a reason for the ban!");
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
-  
-  if(command === "purge") {
-  
-    const deleteCount = parseInt(args[0], 10);
-    
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-    }
-  
 });
 
 client.login(process.env.BOT_TOKEN);
